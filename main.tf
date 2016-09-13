29597deab34fc11260897e11c9034b4b9110e126
Elliot5849 <elliot.marsh@qa.com>
2016-09-13 14:59:15 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ variable "aws_region" {
 
 provider "aws" {
   access_key = "${var.aws_access_key}" 
-  secret_key = "${var.aws_secret_key}" 
+  secret_key = "${var.aws _secret_key}" 
   region     = "${var.aws_region}"
 }
 
